2fc74ab7ebdb540b91b73522b7049c5783601e43
Dmitriy <315overdose@gmail.com>
2023-10-19 11:56:32 +0300

    numberInput styles

--- src/pages/StartGameScreen/ui.tsx
+++ src/pages/StartGameScreen/ui.tsx
@@ -6,7 +6,7 @@ export const StartGameScreen = () => {
   return (
     //TODO need to feature/widgets ?
     <View style={styles.inputContainer}>
-      <Input />
+      <Input style={styles.numberInput} maxLength={2} />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -25,5 +25,15 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    textAlign: 'center',
   },
 })
